0c599c456f0cde04749963c1950eb4ca49a82378
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-03-02 15:42:55 +0100

    trapezium.js

---
+++ 02-JSCoreConcepts/trapezium.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = 2;
+let c = 2;
+let d = 2;
+let h = 1;
+const f = (a,b,h) => {return h*(a+b)/2}
+console.log(`Pole powierzchni trapezu o wymiarach ${a}, ${b}, ${h}, wynosi ${f(a,b,h)}`);
