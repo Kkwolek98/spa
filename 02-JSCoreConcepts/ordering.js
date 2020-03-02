0321ffd1ec0df5ca6070695147d3befce73b2a86
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-03-02 15:49:36 +0100

    ordering.js

--- 02-JSCoreConcepts/ordering.js
+++ 02-JSCoreConcepts/ordering.js
@@ -6,20 +6,20 @@ const reverse = function(arr) {
 }
 
 const sort = function(arr) {
-    arrSorted = [];
-    // ...
+    let arrSorted = [];
+    arrSorted = arr.sort(); 
     return arrSorted;
 }
 
 const even = function(arr) {
     let arrEven = [];
-    // ...
+    arrEven = arr.filter(el => {return (el % 2 === 0)});
     return arrEven;
 }
 
-numbers = [8,3,2,1,5,4,6];
-console.log(numbers);
-console.log(reverse(numbers));
-console.log(even(numbers));
-console.log(reverse(even(numbers)));
-console.log(even(sort(numbers)));
+let numbers = [8,3,2,1,5,4,6];
+console.log("nums: ", numbers);
+console.log("rev: ", reverse(numbers));
+console.log("even: ", even(numbers));
+console.log("rev even: ", reverse(even(numbers)));
+console.log("even sort: ", even(sort(numbers)));
