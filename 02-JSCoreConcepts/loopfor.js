2475704a7f170584ecc4b404a303aa83e540dbe5
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-03-02 12:30:04 +0100

    loop.js

---
+++ 02-JSCoreConcepts/loopfor.js
@@ -0,0 +1,3 @@
+let arr = [2, 6, 10, 14];
+
+for(let i = 0, n=arr.length; i<n; i++) console.log(arr[i])
---
+++ 02-JSCoreConcepts/loopwhile.js
@@ -0,0 +1,12 @@
+// console.log(2);
+// console.log(6);
+// console.log(10);
+// console.log(14);
+
+let arr = [2, 6, 10, 14];
+
+let i = 0;
+while(i<arr.length) {
+    console.log(arr[i]);
+    i++
+}
